757e4d82bee24033893a4f9c0e345e9109cf0b9a
OBlackmon <35784598+OBlackmon@users.noreply.github.com>
2018-03-19 17:55:01 -0400

    Update happyFace.h

--- Eyes/happyFace/happyFace.h
+++ Eyes/happyFace/happyFace.h
@@ -8,10 +8,10 @@ class happyFace : public expression {
 	
 		happyFace();
 		
-		void display(LedControl lc, int iterator, bool direction);
+		void display(LedControl lc);
 		
 	private:
-		void displayPupil(LedControl lc, int iterator, bool direction);
+		void displayPupil(LedControl lc);
 		
 		int pupil[8] = {254, 129, 129, 129, 157, 157, 157, 254};
 };
