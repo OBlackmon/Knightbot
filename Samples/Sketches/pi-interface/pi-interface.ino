33a0a94bee9355c02b6599106da8ebf691a51cf3
TreBlack <blackmoniii.odell@gmail.com>
2018-03-28 20:43:02 -0400

    Changes to pi-interface

--- Samples/Sketches/pi-interface/pi-interface.ino
+++ Samples/Sketches/pi-interface/pi-interface.ino
@@ -1,3 +1,7 @@
+#include <Servo.h>
+
+#include <Head.h>
+
 #include <happyFace.h>
 
 #include <errorFace.h>
@@ -5,6 +9,18 @@
 #include <blink001.h>
 
 #include <LedControl.h>
+
+#define E1 10  // Enable Pin for motor 1
+
+#define E2 11  // Enable Pin for motor 2
+
+#define I1 8  // Control pin 1 for motor 1
+
+#define I2 9  // Control pin 2 for motor 1
+
+#define I3 12  // Control pin 1 for motor 2
+
+#define I4 13  // Control pin 2 for motor 2
 
 /*
   This arduino file was created to accept a specific input from a raspberry pi. The current interface involves receiving values between
@@ -14,9 +30,12 @@ int incomingByte = 0;
 
 int pupilBlink[8] = {255, 255, 255, 255, 255, 255, 255, 255};
 
-int DIN = 5;
-int CS = 4;
-int CLK = 3;
+int trigPin = 6;
+int echoPin = 7;
+
+int DIN = 52;
+int CS = 51;
+int CLK = 50;
 
 int eye_selection = 0;
 int head_selection = 0;
@@ -27,17 +46,35 @@ int wheel_selection = 0;
 */
 
 LedControl lc = LedControl(DIN, CLK, CS, 2);
+Servo horiz = Servo();
+Servo vertic = Servo();
+
 blink001 blink1 = blink001();
 happyFace happy = happyFace();
 errorFace error = errorFace();
+Head head = Head();
 
 void setup() {
   // put your setup code here, to run once:
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(E1, OUTPUT);
+  pinMode(E2, OUTPUT);
+  pinMode(I1, OUTPUT);
+  pinMode(I2, OUTPUT);
+  pinMode(I3, OUTPUT);
+  pinMode(I4, OUTPUT);
+  
   lc.shutdown(0, false);
   lc.setIntensity(0, 0.1);
 
   lc.shutdown(1, false);
   lc.setIntensity(1, 0.1);
+
+  horiz.attach(4);
+  vertic.attach(5);
 
   Serial.begin(9600);
 }
@@ -57,7 +94,7 @@ void loop() {
     head_selection = Serial.parseInt();
     wheel_selection = Serial.parseInt();
   }
-
+ 
   /*
     This switch statement is being used to intake the input values parsed from the Rasberry Pi
     and then running the various combinations of numbers to convery specific eyes, head movements, and wheel movements
@@ -101,14 +138,17 @@ void loop() {
 
     case 1:
 
+      head.turnRight(horiz);
       break;
 
     case 2:
 
+      head.turnLeft(horiz);
       break;
 
     case 3:
 
+      head.nod(vertic);
       break;
 
     case 4:
@@ -118,11 +158,53 @@ void loop() {
 
   switch (wheel_selection) {
     case 0:
+ long duration, distance;
+ digitalWrite(trigPin,HIGH);
+ delayMicroseconds(1000);
+ digitalWrite(trigPin, LOW);
+ duration=pulseIn(echoPin, HIGH);
+ distance =(duration/2)/29.1;
+ Serial.print(distance);
+ Serial.println("CM");
+ delay(10);
 
+  if ((distance <= 45)) {
+    
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+    digitalWrite(I1, LOW);
+    digitalWrite(I2, HIGH);
+    digitalWrite(I3, LOW);
+    digitalWrite(I4, HIGH);
+   }
+    
+    else if (distance > 10) {
+    
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+    digitalWrite(I1, LOW);
+    digitalWrite(I2, HIGH);
+    digitalWrite(I3, HIGH);
+    digitalWrite(I4, LOW);
+    
+   }
+
+  
+
+  delay(500);
+    
+      
       break;
 
     case 1:
-
+        digitalWrite(E1, HIGH);
+        digitalWrite(E2, HIGH);
+        digitalWrite(I1, HIGH);
+        digitalWrite(I2, HIGH);
+        digitalWrite(I3, HIGH);
+        digitalWrite(I4, HIGH);
       break;
 
     case 2:
@@ -138,9 +220,15 @@ void loop() {
       break;
   }
 
+  
+
   eye_selection = 0;
   head_selection = 0;
-  wheel_selection = 0;
+
+
+
+    //TJ's code incoming
+  
 
 }
 
