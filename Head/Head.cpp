70baf1f8c9dca0ead247432fc76e0f146846dcdc
OBlackmon <35784598+OBlackmon@users.noreply.github.com>
2018-04-02 18:25:19 -0400

    Changes made to Head

--- Head/Head.cpp
+++ Head/Head.cpp
@@ -57,35 +57,4 @@ void Head::nod(Servo vertic){
   	delay(500);
   	vertic.write(55);
 }
-/*
-Replace all instances of "Head" with your desired class name
-*/
-#ifndef Head_h
-#define	Head_h
 
-
-class Head {
-	
-	/*
-	Inside of public, define methods that will be used in the main Arduino file. For instance, display() is the method calledi in the main loop.
-	It takes arguments of LedControl, iterator, and direction. You can edit how the method will work inside of the cpp file.
-	*/
-	public:
-	
-		Head();
-		
-		void turnRight(Servo horiz);
-		
-		void turnLeft(Servo horiz);
-		
-		void nod(Servo vertic);
-		
-	/*
-	Inside of private, define methods that will only be used inside of the cpp file. 
-	*/	
-	private:
-	
-};
-
-#endif
->>>>>>> cb757f08fd7b73f3f718534cb5ef1c25e61a5439
